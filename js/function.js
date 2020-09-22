cbfd40282de163ad1020d58b5b86546c823baea4
Ronnie Song <ronniesong0809@gmail.com>
2020-09-21 23:13:45 -0700

    fix: remove inline styles

--- js/function.js
+++ js/function.js
@@ -8,18 +8,18 @@ async function fetchTrending(range) {
 
   for (var i in data) {
     if (data[i].name) {
-      var project = "<img src='" + data[i].avatar + "' style='width:15px;height:15px;margin-right:5px;vertical-align:inherit' SameSite/>" + 
+      var project = "<img src='" + data[i].avatar + "'/>" + 
         "<a href='" + data[i].url + "' target='_blank' rel='noopener noreferrer'>" + data[i].author + " / " + data[i].name + "</a> <br/>"
 
       var description = (data[i].description!="" ? '<span>"' + data[i].description + '"</span>' + "<br/>" : "")
 
       var starsForks = "<span class='badge badge-secondary' style='background-color:" + data[i].languageColor + "'>" + data[i].language + "</span> " + 
-        "<i class='far fa-star' style='margin-left:10px'></i> " + data[i].stars + "<span style='color:gray;font-size:10px;>+" + data[i].currentPeriodStars + " today</span> " + 
-        "<i class='fas fa-code-branch' style='margin-left:10px'></i> " + data[i].forks
+        "<i class='far fa-star'></i> " + data[i].stars + "<span id='grayText'>+" + data[i].currentPeriodStars + " today</span> " + 
+        "<i class='fas fa-code-branch'></i> " + data[i].forks
 
       document.getElementById("gh-trending").innerHTML +=
         "<hr/>" + 
-        "<div style='padding-left:15px;padding-right:15px'>" +
+        "<div id='repo'>" +
           project + description + starsForks + 
         "</div>"
     }
