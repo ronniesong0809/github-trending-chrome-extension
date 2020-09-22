961a7bde704e72063f01d634ebd4ed6a846fc446
Ronnie Song <ronniesong0809@gmail.com>
2020-09-21 22:00:39 -0700

    feat: add more info

--- js/function.js
+++ js/function.js
@@ -7,8 +7,10 @@ async function fetchTrending(range) {
   for (var i in data) {
     if (data[i].name) {
       document.getElementById("gh-trending").innerHTML +=
-        "<span class='badge badge-secondary' style='background-color:" + data[i].languageColor + "'>" + data[i].language + "</span> " + data[i].author + " / " + "<a href='" + data[i].url + "' target='_blank' rel='noopener noreferrer'>" + data[i].name + "</a><br/>" +
-        "stars: " + data[i].stars + ", forks: " + data[i].forks + "<hr>"
+        "<hr/><div style='padding-left:15px;padding-right:15px'>" +
+        "<span class='badge badge-secondary' style='background-color:" + data[i].languageColor + "'>" + data[i].language + "</span> <a href='" + data[i].url + "' target='_blank' rel='noopener noreferrer'>" + data[i].author + " / " + data[i].name + "</a><br/>" +
+        '<span>"' + data[i].description + '"</span>' + "<br/>" +
+        "<i class='far fa-star'></i> " + data[i].stars + " (+" + data[i].currentPeriodStars + " today) <i class='fas fa-code-branch' style='margin-left:10px'></i> " + data[i].forks + "</div>"
     }
   }
 }
