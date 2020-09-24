12bc41b19eef9916ed0b88becbe27bc20e722182
Ronnie Song <ronniesong0809@gmail.com>
2020-09-24 01:29:58 -0700

    feat: add tooltip, and more

--- js/function.js
+++ js/function.js
@@ -4,23 +4,39 @@ async function fetchTrending(range) {
   const response = await fetch(url)
   const data = await response.json()
 
+  function dateRange(range) {
+    if (range == "daily")
+      return "today"
+    else if (range == "weekly")
+      return "this week"
+    else
+      return "this month"
+  }
+
   document.getElementById("gh-trending").innerHTML = ""
 
   for (var i in data) {
     if (data[i].name) {
-      var project = "<img src='" + data[i].avatar + "'/>" + 
+      var project = "<a href='https://github.com/" + data[i].author + "' target='_blank' rel='noopener noreferrer'><img src='" + data[i].avatar + "'/></a>" +
         "<a href='" + data[i].url + "' target='_blank' rel='noopener noreferrer'>" + data[i].author + " / " + data[i].name + "</a> <br/>"
 
-      var description = (data[i].description!="" ? '<span>"' + data[i].description + '"</span>' + "<br/>" : "")
+      var description = (data[i].description != "" ? '<span>"' + data[i].description + '"</span>' + "<br/>" : "")
 
-      var starsForks = "<span class='badge badge-secondary' style='background-color:" + data[i].languageColor + "'>" + data[i].language + "</span> " + 
-        "<i class='far fa-star'></i> " + data[i].stars + "<span id='grayText'>+" + data[i].currentPeriodStars + " today</span> " + 
-        "<i class='fas fa-code-branch'></i> " + data[i].forks
+      var starsForks = "<span class='badge badge-secondary' style='background-color:" + data[i].languageColor + "'>" + data[i].language + "</span> " +
+        "<a href='" + data[i].url + "/stargazers' target='_blank' rel='noopener noreferrer'>" +
+        "<i class='fas fa-star'></i><span id='starFork'>" + data[i].stars.toLocaleString() + "</span>" +
+        "</a>" +
+        " <span class='popup' id='grayText'>&uarr;" + data[i].currentPeriodStars.toLocaleString() +
+        "<span class='popupText'>" + data[i].currentPeriodStars + " stars " + dateRange(range) + "</span> " +
+        "</span>" +
+        "<a href='" + data[i].url + "/network/members' target='_blank' rel='noopener noreferrer'>" +
+        "<i class='fas fa-code-branch'></i><span id='starFork'>" + data[i].forks.toLocaleString() + "</span>" +
+        "</a>"
 
       document.getElementById("gh-trending").innerHTML +=
-        "<hr/>" + 
+        "<hr/>" +
         "<div id='repo'>" +
-          project + description + starsForks + 
+        project + description + starsForks +
         "</div>"
     }
   }
